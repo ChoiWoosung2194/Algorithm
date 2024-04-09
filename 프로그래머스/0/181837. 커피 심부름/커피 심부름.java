ba03948a238b5f9dbd815e98f59566f0d1adaddb
woosung <160567735+ChoiWoosung2194@users.noreply.github.com>
2024-04-09 15:12:02 +0900

    [level 0] Title: 커피 심부름, Time: 0.04 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        for(int i = 0; i< order.length; i++){
-            if(order[i].equals("cafelatte") || order[i].equals("icecafelatte") || order[i].equals("cafelatteice") || order[i].equals("hotcafelatte") || order[i].equals("cafelattehot")){
+        for(String x : order){
+            if(x.contains("cafelatte")){
                 answer += 5000;
-            }else if(order[i].equals("iceamericano") || order[i].equals("hotamericano") || order[i].equals("americanohot") || order[i].equals("americanoice") || order[i].equals("americano")){
-                answer += 4500;
             }else{
                 answer += 4500;
             }
